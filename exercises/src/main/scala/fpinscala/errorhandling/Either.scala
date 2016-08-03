ac0f5c3815215c4f10729dbed17d3e43f88b7459
Cody Rioux <crioux@netflix.com>
2016-08-03 09:56:22 -0700

    Completed Chapter 4 exercises.

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,21 +4,49 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+  // Exercise 4.7
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(b) => f(b)
+      case Left(e) => Left(e)
+    }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = 
+    this match {
+      case Right(b) => Right(b)
+      case Left(e) => b
+    }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    (this, b) match {
+      case (Left(e), _) => Left(e)
+      case (_, Left(e)) => Left(e)
+      case (Right(a), Right(bb)) => Right(f(a, bb))
+    }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = sys.error("todo")
+  // Exercise 4.8
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+  es match {
+    case Nil => Right(Nil)
+    case h::t => (f(h).map2(traverse(t)(f)))(_ :: _)
+  }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = sys.error("todo")
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    es match {
+      case Nil => Right(Nil)
+      case h :: t => h.flatMap(head => sequence(t).map(head :: _))
+    }
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,37 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  // Exercise 4.1
+  def map[B](f: A => B): Option[B] = 
+    this match {
+      case None => None
+      case Some(v) => Some(f(v))
+    }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B =
+    this match {
+      case None => default
+      case Some(v) => v
+    }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = 
+    this match {
+      case None => None
+      case Some(v) => f(v)
+    }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    this match {
+      case None => ob
+      case Some(_) => this
+    }
+
+  def filter(f: A => Boolean): Option[A] = 
+    this match {
+      case None => None
+      case Some(v) => if (f(v)) this else None
+    }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +60,63 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  // Exercise 4.2
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs).flatMap(mu => mean(xs.map(x => Math.pow(x - mu, 2))))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  // Exercise 4.3
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = 
+    (a, b) match {
+      case (None, _) => None
+      case (_, None) => None
+      case (Some(x), Some(y)) => Some(f(x, y))
+    }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  // Exercise 4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = 
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => h.flatMap(head => sequence(t).map(head :: _))
+    }
+
+  // Exercise 4.5
+  // I want to take a list of A and a fn of A => Option[B] and receive an Option[List[B]]
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a match {
+      case Nil => Some(Nil)
+      case h::t => map2(f(h), traverse(t)(f))(_ :: _)
+    }
+}
+
+object ChapterFour {
+
+  import Option._
+
+  def main(args: Array[String]): Unit = {
+
+    // 4.1
+    assert(Some(3).map(x => x + 1) == Some(4))
+    assert(Some(3).getOrElse(4) == 3)
+    assert(None.getOrElse(4) == 4)
+    assert(Some(3).flatMap(x => Some(x + 4)) == Some(7))
+    assert(Some(3).orElse(Some(4)) == Some(3))
+    assert(None.orElse(Some(4)) == Some(4))
+    assert(Some(3).filter(x => x < 5) == Some(3))
+    assert(Some(3).filter(x => x > 5) == None)
+
+    assert(1 == 1) // 4.2
+
+    // 4.3
+    val f = (x: Int, y: Int) => x + y
+    assert(map2(None, None)(f) == None)
+    assert(map2(Some(3), None)(f) == None)
+    assert(map2(None, Some(3))(f) == None)
+    assert(map2(Some(3), Some(4))(f) == Some(7))
+
+    // 4.4
+    val l1 = List(Some(1), Some(2), Some(3))
+    val l2 = List(Some(1), None, Some(3))
+    assert(sequence(l1) == Some(List(1, 2, 3)))
+    assert(sequence(l2) == None)
+  }
 }
